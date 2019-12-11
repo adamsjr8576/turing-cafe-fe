7a9a27c08e1b14bc2c35edad7a04249ca670281a
John Adams <adamsjr8576@gmail.com>
2019-12-11 10:03:43 -0700

    Implement controlled form to update state on change of inputs

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -4,39 +4,59 @@ import './Form.css';
 class Form extends Component {
   constructor(props) {
     super(props);
-
+    this.state = {
+      name: '',
+      date: '',
+      time: '',
+      number: 0
+    }
   }
 
+  updateInputState = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  clearState = () => {
+    this.setState({ name:'', date:'', time:'', number:0 })
+  }
+
+
+
   render() {
+    const { name, date, time, number } = this.state;
     return (
       <form className='reservation-form'>
         <input
           type='text'
-          name=''
-          value=''
+          name='name'
+          value={name}
           placeholder='Name'
           className='form-input'
+          onChange={(event) => this.updateInputState(event)}
         />
         <input
           type='date'
-          name=''
-          value=''
+          name='date'
+          value={date}
           placeholder='Check-in Date'
           className='form-input'
+          onChange={(event) => this.updateInputState(event)}
           />
         <input
           type='text'
-          name=''
-          value=''
+          name='time'
+          value={time}
           placeholder='Check-In Time'
           className='form-input'
+          onChange={(event) => this.updateInputState(event)}
         />
         <input
           type='number'
-          name=''
-          value=''
+          name='number'
+          value={number}
           placeholder='Number of Guests'
           className='form-input'
+          onChange={(event) => this.updateInputState(event)}
         />
         <button type='button' className='reserve-button'>Make Reservation</button>
       </form>
--- src/Components/ReservationCard/ReservationCard.js
+++ src/Components/ReservationCard/ReservationCard.js
@@ -3,8 +3,12 @@ import './ReservationCard.css';
 
 const ReservationCard = ({ date, id, name, number, time }) => {
   return (
-    <article>
-      
+    <article className='reservation-card'>
+      <h2>{name}</h2>
+      <p>{date}</p>
+      <p>{time} PM</p>
+      <p>Number Of Guest: {number}</p>
+      <button className='delete-button'>Cancel</button>
     </article>
   )
 }
