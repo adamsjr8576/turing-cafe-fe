ef11ebe2e54391f5edb0afab33ca2b44a48e48f3
John Adams <adamsjr8576@gmail.com>
2019-12-11 12:05:16 -0700

    Test that reservationdelete method is being called

--- src/Components/ReservationCard/ReservationCard.test.js
+++ src/Components/ReservationCard/ReservationCard.test.js
@@ -5,17 +5,31 @@ import { shallow } from 'enzyme';
 
 describe('ReservationCard', () => {
 
-  it('should match the snapshot with all data passing correctly', () => {
-    const wrapper = shallow(<ReservationCard
+  let wrapper;
+  const mockhandleReservationDelete = jest.fn();
+
+  beforeEach(() => {
+    wrapper = shallow(<ReservationCard
       date='12/5'
       id='123123'
       name='John Adams'
       number={4}
       time='4:00'
       key='123123'
+      handleReservationDelete={mockhandleReservationDelete}
       />);
+  });
 
+  it('should match the snapshot with all data passing correctly', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should call handleReservationDelete on button click', () => {
+    const mockEvent = {target: {id: 212312}}
+
+    wrapper.find('button').simulate('click', mockEvent);
+
+    expect(mockhandleReservationDelete).toHaveBeenCalledWith(212312);
   })
 
 });
--- src/Components/ReservationContainer/ReservationContainer.test.js
+++ src/Components/ReservationContainer/ReservationContainer.test.js
@@ -5,9 +5,14 @@ import { shallow } from 'enzyme';
 
 describe('ReservationContainer', () => {
 
+  const mockhandleReservationDelete = jest.fn();
+  const mocksortReservations = jest.fn();
+
   it('should match the snapshot with all data passing correctly', () => {
     const wrapper = shallow(<ReservationContainer
       reservations={[{date:'12/5', name:'John Adams', number:4, time:'4:00'}]}
+      handleReservationDelete={mockhandleReservationDelete}
+      sortReservations={mocksortReservations}
       />);
 
     expect(wrapper).toMatchSnapshot();
